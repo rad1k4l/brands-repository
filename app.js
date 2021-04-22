269f66ac23addee8e37d6c4da7d44aac8cb50332
rad1k4l <zeynalli169@gmail.com>
2021-04-22 21:09:42 +0400

    use listener address information from config file

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const indexRouter = require('./routes/index');
+const {conf : config} = require('./conf')
 
 const app = express()
 const port = 3000
@@ -22,6 +23,6 @@ app.use(bodyParser.json());
 app.use('/', indexRouter);
 
 
-app.listen(`${port}`, () => {
-    console.log(`App listening at http://localhost:${port}`)
+app.listen(config.listen.port, config.listen.ip, () => {
+    console.log(`App listening at ${config.listen.ip}:${config.listen.port}`)
 })
