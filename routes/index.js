38311c9df093c94f5814644c8c33c50c7aaf019d
rad1k4l <zeynalli169@gmail.com>
2021-04-22 18:26:45 +0400

    integrated googlesearch

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,46 @@ const router = express.Router();
 const {URL} = require('url');
 const {scan} = require('../services/logofetch/service')
 
+const messages = require('../generated/protos/google_pb');
+const services = require('../generated/protos/google_grpc_pb');
+
+const grpc = require('@grpc/grpc-js');
 
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Brand fetch by url'});
 });
 
 router.post('/search/domain', async function (req, res, next) {
+
     if (!req.body.domain) {
         res.status(400).json({msg: 'validation error. please provide domain'}).end()
         return;
     }
+    const searchQuery = req.body.domain;
+
+    let client = new services.GoogleSearcherClient("localhost:42420", grpc.credentials.createInsecure());
+    let request = new messages.GoogleSearchRequest();
+    request.setQuery(searchQuery);
+    const results = await new Promise(resolve => {
+        client.search(request, function (err, response) {
+            resolve(response.getResultsList());
+        });
+    });
+
+    let domain = results[0];
+    if (!domain) {
+        res.status(400).json({msg: 'validation error. please provide domain'}).end()
+        return;
+    }
     try {
-        new URL(req.body.domain)
+        new URL(domain)
     } catch (e) {
         res.status(400).json({msg: 'validation error. please provide a valid domain'}).end()
         return;
     }
 
     try {
-        res.status(200).json(await scan(req.body.domain)).end();
+        res.status(200).json(await scan(domain)).end();
     } catch (e) {
         res.status(400).json({msg: 'Error ocurred'}).end();
         throw e;
